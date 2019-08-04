cae2381abc2371261e48108a9629868074d97468
arkadiy <arkadiiozerov13@gmail.com>
2019-08-04 12:00:19 +0300

    implemented use cases for task

--- manager/src/Model/Work/Entity/Projects/Role/Permission.php
+++ manager/src/Model/Work/Entity/Projects/Role/Permission.php
@@ -9,6 +9,8 @@ use Webmozart\Assert\Assert;
 class Permission
 {
     public const MANAGE_PROJECT_MEMBERS = 'manage_project_members';
+    public const VIEW_TASKS = 'view_tasks';
+    public const MANAGE_TASKS = 'manage_tasks';
     private $name;
 
     public function __construct(string $name)
@@ -21,6 +23,8 @@ class Permission
     {
         return [
             self::MANAGE_PROJECT_MEMBERS,
+            self::VIEW_TASKS,
+            self::MANAGE_TASKS,
         ];
     }
 
---
+++ manager/src/Model/Work/Entity/Projects/Task/TaskRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\Entity\Projects\Task;
+
+use App\Model\EntityNotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TaskRepository
+{
+    private $repo;
+    private $connection;
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->repo = $em->getRepository(Task::class);
+        $this->em = $em;
+        $this->connection = $em->getConnection();
+    }
+
+    public function get(Id $id): Task
+    {
+        /** @var Task $task */
+        if (!$task = $this->repo->find($id->getValue())) {
+            throw new EntityNotFoundException('Task is not found.');
+        }
+        return $task;
+    }
+
+    public function add(Task $task): void
+    {
+        $this->em->persist($task);
+    }
+
+    public function nextId(): Id
+    {
+        return new Id((int)$this->connection->query('SELECT nextval(\'work_projects_tasks_seq\')')->fetchColumn());
+    }
+
+    public function remove(Task $task): void
+    {
+        $this->em->remove($task);
+    }
+
+    /**
+     * @param Id $id
+     * @return Task[]
+     */
+    public function allByParent(Id $id): array
+    {
+        return $this->repo->findBy(['parent' => $id->getValue()]);
+    }
+
+}
